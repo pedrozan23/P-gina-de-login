380f57df123a57af1bdbafed08322792bd5833c8
Pedro Zanetti <pzanetti868@gmail.com>
2026-09-16 21:58:32 -0300

    Add event listeners for login and register buttons
    Added event listeners for login and register buttons to toggle active class on the container.

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 const container = document.getElementById('container');
 const loginBtn = document.getElementById('login');
 const registerBtn = document.getElementById('registro');
+
+registerBtn.addEventListener('click', () => {
+    container.classList.add('active');
+});
+
+loginBtn.addEventListener('click', () => {
+    container.classList.remove('active');
+});
